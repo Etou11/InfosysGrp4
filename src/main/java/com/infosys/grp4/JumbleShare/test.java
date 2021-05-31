4f51abe3e28090f12254fec8de3212eb2f1a3547
Etou11 <74773265+Etou11@users.noreply.github.com>
2021-05-31 22:20:54 +0200

    Fixed some git related issues

--- src/main/java/com/infosys/grp4/JumbleShare/test.java
+++
@@ -1,8 +0,0 @@
-package hsesslingen.group4.jumbleShare;
-
-public class test {
-    public void testclass(){
-        System.out.println("hello world")   ;
-    }
-
-}
---
+++ src/test/java/com/infosys/grp4/JumbleShare/JumbleShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.grp4.JumbleShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumbleShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
