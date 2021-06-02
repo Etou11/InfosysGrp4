d5fca6d251e0523f468280e238553a4ecd475f54
Etou11 <74773265+Etou11@users.noreply.github.com>
2021-06-02 16:33:39 +0200

    Prototyping

--- src/main/java/com/infosys/grp4/JumbleShare/MainController.java
+++ src/main/java/com/infosys/grp4/JumbleShare/MainController.java
@@ -1,8 +1,12 @@
 package com.infosys.grp4.JumbleShare;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Controller
 public class MainController
@@ -19,4 +23,21 @@ public class MainController
     {
         return "index2";
     }
+
+    @GetMapping("/test")
+    public String test(Model model)
+    {
+        return "test";
+    }
+
+    @Autowired
+    FetchDataService fetchDataService;
+
+    @GetMapping(path = "getData")
+    List<UserModel> getUsers()
+    {
+        return fetchDataService.findAll();
+    }
+
+
 }
