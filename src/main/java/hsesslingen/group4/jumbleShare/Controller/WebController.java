3253a018e7e2aef1f3fe77419016a4fffb5e64cd
Etou11 <74773265+Etou11@users.noreply.github.com>
2021-06-15 16:16:54 +0200

    Fixed paths of html files and controller

--- src/main/java/hsesslingen/group4/jumbleShare/Controller/WebController.java
+++ src/main/java/hsesslingen/group4/jumbleShare/Controller/WebController.java
@@ -7,33 +7,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-    @RequestMapping(value = "/index")
+    @RequestMapping(value = "/index.html")
     public String index() {
         return "index";
     }
 
-    @GetMapping(value = "/account")
+    @GetMapping(value = "/account.html")
     public String account()
     {
         return "account";
     }
 
-    @GetMapping(value = "/contact")
+    @GetMapping(value = "/contact.html")
     public String contact()
     {
         return "contact";
     }
 
-    @GetMapping(value = "/login")
+    @GetMapping(value = "/login.html")
     public String login()
     {
         return "login";
     }
 
-    @GetMapping(value = "/new")
+    @GetMapping(value = "/new.html")
     public String newPage() { return "new"; }
 
-    @GetMapping(value = "/test")
+    @GetMapping(value = "/test.html")
     public String test()
     {
         return "test";
