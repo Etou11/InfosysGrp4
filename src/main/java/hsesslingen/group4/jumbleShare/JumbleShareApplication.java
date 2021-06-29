32c9df3af26288f405f643043295c950d6f2aa42
Etou11 <74773265+Etou11@users.noreply.github.com>
2021-06-29 17:26:09 +0200

    Test push due to GitLab

--- src/main/java/hsesslingen/group4/jumbleShare/JumbleShareApplication.java
+++ src/main/java/hsesslingen/group4/jumbleShare/JumbleShareApplication.java
@@ -8,7 +8,7 @@ import java.util.UUID;
 @SpringBootApplication
 public class JumbleShareApplication {
 
-	//TODO: Replace with an actual authentication system - use for development only! Replace any occurrences (Ctrl + Shift + F) of this id when changing!
+	//TODO: Replace with an actual authentication system - use for development only! Replace any occurrences (Ctrl + Shift + F) of this id when changing!!
 	public static String currentlyActiveUser = "D568817B-A8D2-43AA-A4BD-DC92378CB6DE";
 	public static UUID _currentlyActiveUser = UUID.fromString(currentlyActiveUser);
 
